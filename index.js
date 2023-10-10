510027a131e3c3844f561ae38c777900deb1425c
semio Ds <semiod@gmail.com>
2023-10-10 18:12:43 +0100

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
 let notes = [
   {
     id: 1,
@@ -78,7 +81,7 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
